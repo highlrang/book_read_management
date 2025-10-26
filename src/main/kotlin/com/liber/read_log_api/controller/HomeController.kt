fe8749f8764ef089252b86760bbb43b0d8739d9e
hwjeong-dm <hwjeong@digitalmedic.ai>
2025-10-26 19:15:39 +0900

    fix. db url

--- src/main/kotlin/com/liber/read_log_api/controller/HomeController.kt
+++ src/main/kotlin/com/liber/read_log_api/controller/HomeController.kt
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 class HomeController {
 
-    @Value("\${MYNAME:GUEST}")
+    @Value("\${NAME:GUEST}")
     private lateinit var NAME: String
 
     @GetMapping
