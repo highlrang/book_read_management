62561443e03acec20b10bc8dafa8baff3df80198
highlrang <59989932+highlrang@users.noreply.github.com>
2025-11-16 16:44:11 +0900

    fix. colum

--- src/main/kotlin/com/liber/book_read_management/entities/User.kt
+++ src/main/kotlin/com/liber/book_read_management/entities/User.kt
@@ -13,11 +13,16 @@ class User (
     var loginId: String = "",
     var password: String = "",
     var name: String = "",
+    @Column(name = "phone_number")
     var phoneNumber: String = "",
     var address: String? = null,
+    @Column(name = "address_detail")
     var addressDetail: String? = null,
+    @Column(name = "address_latitude")
     var addressLatitude: Double? = null,
+    @Column(name = "address_longitude")
     var addressLongitude: Double? = null,
+    @Column(name = "access_token")
     var accessToken: String? = null,
 
 ) : BaseTimeEntity()
