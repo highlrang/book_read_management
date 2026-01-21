e14cb11f9459b39435ad7a233f6b8caee18eebb4
highlrang <59989932+highlrang@users.noreply.github.com>
2026-01-21 22:22:17 +0900

    fix. logback json

--- build.gradle.kts
+++ build.gradle.kts
@@ -72,6 +72,8 @@ dependencies {
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+    implementation("net.logstash.logback:logstash-logback-encoder:7.4")
 }
 
 dependencyManagement {
