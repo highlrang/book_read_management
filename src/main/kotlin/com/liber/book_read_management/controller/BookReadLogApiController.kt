abb7121ab6b3436bd4a605fb89e9dc9e5da85efa
highlrang <59989932+highlrang@users.noreply.github.com>
2026-03-19 19:19:26 +0900

    fix. 서재 API

--- src/main/kotlin/com/liber/book_read_management/controller/BookReadLogApiController.kt
+++ src/main/kotlin/com/liber/book_read_management/controller/BookReadLogApiController.kt
@@ -15,21 +15,21 @@ import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
-@Tag(name = "도서 읽기 기록 API", description = "도서 읽기 기록 관련 API")
+@Tag(name = "내 서재 API", description = "내 서재 관련 API")
 @RestController
 @RequestMapping("/api/v1/book-read")
 class BookReadLogApiController (
     var bookReadLogService: BookReadLogService
 ) {
 
-    @Operation(summary = "도서 읽기 기록 저장", description = "도서 읽기 기록을 저장합니다.")
+    @Operation(summary = "내 서재에 추가", description = "도서를 내 서재에 추가한다.")
     @PostMapping
     fun saveReadLog(@CurrentUserId userId: Long, @Valid @RequestBody readLogSaveRequest: BookReadLogSaveRequest) : ResponseEntity<ApiResponse<BookReadLogResponse>> {
         val bookReadLogResponse = bookReadLogService.saveBookReadLog(userId, readLogSaveRequest)
         return ResponseEntity.ok(ApiResponse.success(bookReadLogResponse))
     }
 
-    @Operation(summary = "도서 읽기 기록 목록 조회", description = "도서 읽기 기록 목록을 조회합니다.")
+    @Operation(summary = "내 서재 목록 조회", description = "내 서재 목록을 조회합니다.")
     @GetMapping
     fun getBookReadLogs(@CurrentUserId userId: Long,
                         @ParameterObject request: BookReadLogSearchRequest
@@ -38,7 +38,7 @@ class BookReadLogApiController (
         return ResponseEntity.ok(ApiResponse.success(bookReadLogResponseList))
     }
 
-    @Operation(summary = "도서 읽기 기록 상세 조회", description = "도서 읽기 기록 상세 정보를 조회합니다.")
+    @Operation(summary = "내 서재 도서 상세 조회", description = "저장된 서재 상세 아이디로 도서 기록 상세 정보를 조회합니다.")
     @GetMapping("/{bookReadLogId}")
     fun getBookReadLog(@CurrentUserId userId: Long,
                        @PathVariable bookReadLogId: Long) : ResponseEntity<ApiResponse<BookReadLogDetailResponse>> {
